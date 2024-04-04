c3b3927c5030d526257c14c6861b695b3ad38451
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-04 12:34:21 +0200

    ✨Module 2 finished

--- DisplayRoutes/BusRoute.cs
+++ DisplayRoutes/BusRoute.cs
@@ -4,17 +4,17 @@ using System.Text;
 
 namespace Pluralsight.ArraysCollections.Demos
 {
-	public class BusRoute
-	{
-		public int Number { get; }
-		public string Origin { get; }
-		public string Destination { get; }
-		public BusRoute(int number, string origin, string destination)
-		{
-			this.Number = number;
-			this.Origin = origin;
-			this.Destination = destination;
-		}
-		public override string ToString() => $"{Number}: {Origin} -> {Destination}";
-	}
+    public class BusRoute
+    {
+        public int Number { get; }
+        public string Origin { get; }
+        public string Destination { get; }
+        public BusRoute(int number, string origin, string destination)
+        {
+            this.Number = number;
+            this.Origin = origin;
+            this.Destination = destination;
+        }
+        public override string ToString() => $"{Number}: {Origin} -> {Destination}";
+    }
 }
